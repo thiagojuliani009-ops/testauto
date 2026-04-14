559b1aeed2ded23171f7a9f3e9ff0c9bca80aaeb
THIAGO JULIANI DE OLIVEIRA <thiagojuliani009@gmail.com>
2026-04-14 14:25:39 -0300

    atualizar comprar_contato

--- script/engine.js
+++ script/engine.js
@@ -1,4 +1,16 @@
-function toggleMenu() {
-  const menu = document.getElementById("menu");
-  menu.classList.toggle("active");
+class navbar{
+    constructor(menuIcon, navLinks){
+        this.menuIcon = menuIcon;
+        this.navLinks = navLinks;
+    }
 }
+
+addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.querySelector('.menu-icon');
+    const navLinks = document.querySelector('.nav-links');
+    const navBar = new navbar(menuIcon, navLinks);
+    navBar.menuIcon.addEventListener('click', () => {
+        navBar.navLinks.classList.toggle('active');
+    });
+});
+
